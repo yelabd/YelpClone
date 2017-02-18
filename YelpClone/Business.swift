bb1f7dedf475021ae12b6748820244c424336d1f
Youssef <yelabd@purdue.edu>
2017-02-18 18:44:42 -0500

    Added initial classes

---
+++ YelpClone/Business.swift
@@ -0,0 +1,13 @@
+//
+//  Business.swift
+//  YelpClone
+//
+//  Created by Youssef Elabd on 2/18/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class Business: NSObject {
+
+}
---
+++ YelpClone/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  YelpClone
+//
+//  Created by Youssef Elabd on 2/18/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
